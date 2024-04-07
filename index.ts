92d4fb661a89f434c46afe8e670492b677d210b9
Mykhan99 <160237773+Mykhan99@users.noreply.github.com>
2024-04-07 12:56:16 -0700

    My first upload

---
+++ index.ts
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+const answers = await inquirer.prompt([
+{
+    name: "userGuessedNumber",
+    type: "number",
+    message: "Please guess a number 1 TO 6: ",
+    }
+]);
+
+const randomNumber = Math.floor(Math.random() * 6+1);
+console.log("Computer Generated Number:"),
+console.log(randomNumber);
+
+if(answers.userGuessedNumber === randomNumber) {
+    console.log("Congradulation you guessed the right number.");
+} else {
+    console.log("You guessed wrong nnumber.");     
+
+}
